193158314cfd8656f95571b754c0fe876d6ee88d
zkn <zkn@reichl.cc>
2023-10-23 21:24:45 +0200

    search item component and styles

--- react_series/01tut/src/App.js
+++ react_series/01tut/src/App.js
@@ -1,4 +1,5 @@
 import Header  from './Header';
+import SearchItem from "./SearchItem";
 import AddItem from "./AddItem";
 import Content from './Content';
 import Footer from './Footer';
@@ -7,8 +8,8 @@ import {useState} from "react";
 
 function App() {
     const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
-
     const [newItem, setNewItem] = useState('');
+    const [search, setSearch] = useState('');
 
     const setAndSaveItems = (newItems) => {
         setItems(newItems);
@@ -47,8 +48,12 @@ function App() {
                 setNewItem={setNewItem}
                 handleSubmit={handleSubmit}
             />
+            <SearchItem
+                search={search}
+                setSearch={setSearch}
+            />
             <Content
-                items={items}
+                items={items.filter(item => ((item.item).toLowerCase().includes(search.toLowerCase())))}
                 handleCheck={handleCheck}
                 handleDelete={handleDelete}
             />
